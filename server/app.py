df30381b2764c1f1ce54bc8b5a4ee259da483d46
Sam Waters <samuel.waters@gmail.com>
2023-10-12 14:21:10 -0600

    TEAMWORK

--- server/app.py
+++ server/app.py
@@ -14,3 +14,73 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+@app.get('./bakeries')
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    data = [bakeries.to_dict(only=('id', 'name', 'address'))for bakeries in bakeries]
+
+    return make_response(
+        jsonify(data),
+        200
+    )
+
+@app.get('/bakeries/<int:id>')
+def get_bakeries_by_id(id):
+    bakery = Bakery.query.filter(
+        Bakery.id == id
+    ).first()
+
+    if not bakery:
+        return make_response(
+            jsonify({'error': 'bakery not found'}),404
+        )
+    
+    return make_response(
+        jsonify(bakery.to_dict(only = ('id','name','address','cakes')),200)
+    )
+
+@app.delete('/bakeries/<int:id>')
+def delete_bakery(id):
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    if not bakery:
+        return make_response(
+            jsonify({'error': 'bakery not found'}), 404
+        )
+    db.session.delete(bakery)
+    db.session.commit()
+
+    return make_response(jsonify({}), 204)
+
+@app.get('/cakes')
+def get_cakes():
+    cake = [c.to_dict(only=('id','name','description')) for c in Cake.query.all()]
+    return make_response(cake, 200)
+
+@app.post('/cake_bakery')
+def post_bakery():
+    data = request.get_json()
+    try:
+        new_bakery = CakeBakery(
+        price = data['price'],
+        cake_id = data['cake_id'],
+        bakery_id = data['bakery_id']
+        )
+    except Exception:
+        return "error error error"
+    db.session.add(new_bakery)
+    db.session.commit()
+    return make_response(jsonify(new_bakery.to_dict()),200)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,10 +4,58 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Cake(db.Model, SerializerMixin):
+    __tablename__ = 'cake_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description=db.Column(db.String)
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
+
+    cakebakery_c_relationship = db.relationship('CakeBakery', back_populates='cake_relationship')
+
+
+class Bakery(db.Model, SerializerMixin):
+    __tablename__ = 'bakery_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    cakebakery_b_relationship = db.relationship('CakeBakery', back_populates='bakery_relationship')
+
+class CakeBakery(db.Model, SerializerMixin):
+    __tablename__ = 'cakebakery_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+
+    cake_id = db.Column(db.Integer, db.ForeignKey('cake_table.id'))
+    cake_relationship = db.relationship('Cake', back_populates='')
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakery_table.id'))
+    bakery_relationship = db.relationship('Bakery', back_populates='')
+
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if 0 < price <= 1000:
+            return price
+        else:
+            raise ValueError("Price is not correct.")
+
+
+
+
+
+
